0ece7ec675a10a9b8f9218680b18c618b09f722e
bluelovers <codelovers@users.sourceforge.net>
2022-06-06 13:44:38 +0800

    .

--- dist/index.cjs
+++ dist/index.cjs
@@ -2,7 +2,7 @@
 'use strict'
 
 if (typeof process !== 'undefined' && process.env.NODE_ENV === 'production') {
-  module.exports = require('./exceldate2.cjs.production.min.cjs')
+  module.exports = require('./index.cjs.production.min.cjs')
 } else {
-  module.exports = require('./exceldate2.cjs.development.cjs')
+  module.exports = require('./index.cjs.development.cjs')
 }
---
+++ dist/index.d.ts
@@ -0,0 +1,25 @@
+export declare function getExcelEpoch(): Date;
+export declare function getExcelDelta(): number;
+export declare type IExcelDateNumberInput = string | number;
+export declare function validDateNumber(excelDateNumber: number): excelDateNumber is number;
+export declare function excelDateNumber(excelDate: IExcelDateNumberInput): number;
+/**
+ * Convert input to a number
+ */
+export declare function excelDateNumberToTs(excelDate: IExcelDateNumberInput): number;
+export declare function ts2excel(milliseconds: number): number;
+export declare function date2excel(date: Date): number;
+/**
+ * Convert input to JS Date
+ * Details here (mostly in comments): https://gist.github.com/christopherscott/2782634
+ *
+ * @link https://gist.github.com/christopherscott/2782634
+ */
+export declare function exceldateTs<T = number>(excelDate: IExcelDateNumberInput, done?: (err: Error, unixTs?: number) => T): T;
+/**
+ * Takes an Excel timestamp (as a number or string) and returns a corresponding Date object
+ */
+export declare function exceldate<T = Date>(excelDate: IExcelDateNumberInput, done?: (err: Error, res?: Date) => T): T;
+export default exceldate;
+
+export {};
